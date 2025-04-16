aa6adf581271a9341b34c63b6f553c251ed1542f
Germanchrystan <germanchrystangmail.com>
2025-04-15 23:34:53 -0300

    stack

--- 06 Datos complejos/6.py
+++ 06 Datos complejos/6.py
@@ -1,13 +1,36 @@
-# "()", "{}", "[]"
-openingSigns = {"(":")", "{": "}", "[": "]"}
+signs = {"(":")", "{": "}", "[": "]"}
 
-def checkBalance(data):
+def isOpening(char: str) -> bool:
+  return signs.keys().__contains__(char)
+
+def isClosing(char: str) -> bool:
+  return [")", "]", "}"].__contains__(char)
+
+def checkBalance(data: str) -> bool:
   stack = []
-  balanced = True
   for i in range (0, len(data)):
     char = data[i]
-    if openingSigns.keys().__contains__(char):
+    
+    if isOpening(char):
       stack.append(char)
-      for j in range (i, len(data)):
+    
+    if isClosing(char):
+      if len(stack) == 0:
+        return False
+      
+      lastStackItem = stack[len(stack) -1]
+      if signs[lastStackItem] == char:
+        stack.pop()
+      else:
+        return False
+  return len(stack) == 0
 
-"({{}})"
+firstCase = checkBalance("({{}})") # True
+secondCase = checkBalance("({{}})()[]") # True
+thirdCase = checkBalance("({{}})]") # False
+fourthCase = checkBalance("[({{}})") # False
+
+print(firstCase)
+print(secondCase)
+print(thirdCase)
+print(fourthCase)
